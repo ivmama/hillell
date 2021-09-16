0e5a415a17eba774c3e95ec63f3ff4ddd03360f7
ivan <>
2021-09-16 17:54:25 +0300

    some-staff

--- admin-react/admin-react-v1/src/components/app/app.js
+++ admin-react/admin-react-v1/src/components/app/app.js
@@ -1,7 +1,14 @@
 import { Route } from "react-router-dom";
 import Header from "../header";
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />;
+      <Route path="/">
+        {/* <Home></Home> */}
+      </Route>
+    </>
+  );
 };
 
 export default App;
--- admin-react/admin-react-v1/src/components/header/header.js
+++ admin-react/admin-react-v1/src/components/header/header.js
@@ -1,11 +1,31 @@
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const ToolbarWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const Header = () => {
   return (
-      <p>Hello?</p>
+    <AppBar position="static">
+      <ToolbarWrapper>
+        <Toolbar>
+          <Button color="inherit" component={NavLink} to="/" exact>
+            Home
+          </Button>
+          <Button color="inherit" component={NavLink} to="/users">
+            Users
+          </Button>
+          <Button color="inherit" component={NavLink} to="/about">
+            About
+          </Button>
+        </Toolbar>
+      </ToolbarWrapper>
+    </AppBar>
   );
 };
 
